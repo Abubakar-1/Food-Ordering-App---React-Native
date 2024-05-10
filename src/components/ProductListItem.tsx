74f2f590a40e07b21fe9833eab30da32611f5760
Abubakar <abubakaruabdulkadir@gmail.com>
2024-05-10 11:20:06 +0100

    Expo Router Changes

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,55 @@
+import Colors from "@constants/Colors";
+import { Image, StyleSheet, Text, View, Pressable } from "react-native";
+import { Product } from "../types";
+import { Link } from "expo-router";
+
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+export const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <Link href={`/menu/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{ uri: product.image || defaultPizzaImage }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </Pressable>
+    </Link>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+    flex: 1,
+    maxWidth: "50%",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+});
