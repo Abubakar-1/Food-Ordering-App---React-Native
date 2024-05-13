2eb021ce39e2ecc1520a37e9ae4498172005f620
Abubakar <abubakaruabdulkadir@gmail.com>
2024-05-13 17:19:45 +0100

    total price in cart

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -6,12 +6,14 @@ type CartType = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem["size"]) => void;
   updateQuantity: (itemId: string, amount: -1 | 1) => void;
+  total: number;
 };
 
 const CartContext = createContext<CartType>({
   items: [],
   addItem: () => {},
   updateQuantity: () => {},
+  total: 0,
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
@@ -51,8 +53,15 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     );
   };
 
+  //reduce loops through everything and collect it as one value
+  //0 is the initial value
+  const total = items.reduce(
+    (sum, item) => (sum += item.product.price * item.quantity),
+    0
+  );
+
   return (
-    <CartContext.Provider value={{ items, addItem, updateQuantity }}>
+    <CartContext.Provider value={{ items, addItem, updateQuantity, total }}>
       {children}
     </CartContext.Provider>
   );
