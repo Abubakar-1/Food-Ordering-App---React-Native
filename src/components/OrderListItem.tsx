e3009a15696604be941049077f87fc887023628b
Abubakar <abubakaruabdulkadir@gmail.com>
2024-05-22 12:11:07 +0100

    Assignment Order

--- src/components/OrderListItem.tsx
+++ src/components/OrderListItem.tsx
@@ -1,7 +1,11 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import React from "react";
+import { Order } from "../types";
+import relativeTime from "dayjs/plugin/relativeTime";
+import dayjs from "dayjs";
 import { Link, useSegments } from "expo-router";
-import { Order } from "@/types";
+
+dayjs.extend(relativeTime);
 
 type OrderListItemProps = {
   order: Order;
@@ -9,20 +13,40 @@ type OrderListItemProps = {
 
 const OrderListItem = ({ order }: OrderListItemProps) => {
   const segments = useSegments();
-  console.log(segments);
 
   return (
     <Link href={`/${segments[0]}/order/${order.id}`} asChild>
-      <Pressable>
+      <Pressable style={styles.container}>
         <View>
-          <Text>Order #{order.id}</Text>
-          <Text>Order #{order.id}</Text>
+          <Text style={styles.title}>Order #{order.id}</Text>
+          <Text style={styles.time}>{dayjs(order.created_at).fromNow()}</Text>
         </View>
 
-        <Text>{order.status}</Text>
+        <Text style={styles.status}>{order.status}</Text>
       </Pressable>
     </Link>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    marginVertical: 5,
+  },
+  time: {
+    color: "gray",
+  },
+  status: {
+    fontWeight: "500",
+  },
+});
 
 export default OrderListItem;
