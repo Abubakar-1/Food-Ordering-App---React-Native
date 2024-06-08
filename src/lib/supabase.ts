f139418d200218cb8e1f636f6c82a64dec72b455
Abubakar <abubakaruabdulkadir@gmail.com>
2024-06-08 12:51:57 +0100

    Corrected network issues and addes thr trigger function to create a new user in d profiles table after sign up auth

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,6 +3,8 @@ import * as SecureStore from "expo-secure-store";
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "@/database.types";
 
+import Config from "react-native-config";
+
 const ExpoSecureStoreAdapter = {
   getItem: (key: string) => {
     return SecureStore.getItemAsync(key);
@@ -15,9 +17,11 @@ const ExpoSecureStoreAdapter = {
   },
 };
 
-const supabaseUrl = "https://rafddyuminarobmcimjt.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhZmRkeXVtaW5hcm9ibWNpbWp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY3OTc0NDcsImV4cCI6MjAzMjM3MzQ0N30._pUzWY0i7pWqulon8yiW-7mrO1n-44jIHDyqpcemkFk";
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
+console.log(supabaseUrl);
+
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON || "";
+console.log(supabaseAnonKey);
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
